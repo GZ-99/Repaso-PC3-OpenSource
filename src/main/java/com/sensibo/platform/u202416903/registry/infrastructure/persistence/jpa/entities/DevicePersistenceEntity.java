c6bbc77e27c55cc2fb6c89c329029ef31d156e11
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-22 14:10:09 -0500

    feat(infrastructure): add DevicePersistenceEntity.java

---
+++ src/main/java/com/sensibo/platform/u202416903/registry/infrastructure/persistence/jpa/entities/DevicePersistenceEntity.java
@@ -0,0 +1,55 @@
+package com.sensibo.platform.u202416903.registry.infrastructure.persistence.jpa.entities;
+
+import com.sensibo.platform.u202416903.registry.domain.model.valueobjects.DeviceTypes;
+import com.sensibo.platform.u202416903.registry.domain.model.valueobjects.MacAddress;
+import com.sensibo.platform.u202416903.registry.domain.model.valueobjects.SensiboUserId;
+import com.sensibo.platform.u202416903.registry.domain.model.valueobjects.SerialNumber;
+import com.sensibo.platform.u202416903.registry.domain.model.valueobjects.Version;
+import com.sensibo.platform.u202416903.registry.infrastructure.persistence.jpa.converters.MacAddressPersistenceConverter;
+import com.sensibo.platform.u202416903.registry.infrastructure.persistence.jpa.converters.SensiboUserIdPersistenceConverter;
+import com.sensibo.platform.u202416903.registry.infrastructure.persistence.jpa.converters.SerialNumberPersistenceConverter;
+import com.sensibo.platform.u202416903.registry.infrastructure.persistence.jpa.converters.VersionPersistenceConverter;
+import com.sensibo.platform.u202416903.shared.infrastructure.persistence.jpa.entities.AuditableAbstractPersistenceEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "devices")
+@Getter
+@Setter
+public class DevicePersistenceEntity extends AuditableAbstractPersistenceEntity {
+
+    @Convert(converter = SensiboUserIdPersistenceConverter.class)
+    @Column(name = "user_id", nullable = false)
+    private SensiboUserId userId;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "device_type", nullable = false)
+    private DeviceTypes deviceType;
+
+    @Column(name = "model_name", length = 50, nullable = false)
+    private String modelName;
+
+    @Convert(converter = SerialNumberPersistenceConverter.class)
+    @Column(name = "serial_number", nullable = false)
+    private SerialNumber serialNumber;
+
+    @Convert(converter = MacAddressPersistenceConverter.class)
+    @Column(name = "mac_address",  nullable = false)
+    private MacAddress macAddress;
+
+    @Convert(converter = VersionPersistenceConverter.class)
+    @Column(name = "firmware_version",  nullable = false)
+    private Version firmwareVersion;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "installation_date",  nullable = false)
+    private LocalDate installationDate;
+
+    @Column(name = "room_location", nullable = false)
+    private String roomLocation;
+
+}
