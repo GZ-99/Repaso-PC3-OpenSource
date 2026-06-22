fec24595be13313c98daf6c2c90581ef2b8dbbba
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-22 14:27:34 -0500

    feat(domain): update Device.java

--- src/main/java/com/sensibo/platform/u202416903/registry/domain/model/aggregates/Device.java
+++ src/main/java/com/sensibo/platform/u202416903/registry/domain/model/aggregates/Device.java
@@ -1,5 +1,6 @@
 package com.sensibo.platform.u202416903.registry.domain.model.aggregates;
 
+import com.sensibo.platform.u202416903.registry.domain.model.commands.CreateDeviceCommand;
 import com.sensibo.platform.u202416903.registry.domain.model.valueobjects.*;
 import com.sensibo.platform.u202416903.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
 import lombok.Getter;
@@ -27,4 +28,17 @@ public class Device extends AbstractDomainAggregateRoot {
     private LocalDate installationDate;
 
     private String roomLocation;
+
+    public Device() {}
+
+    public Device(CreateDeviceCommand command) {
+        this.userId = new SensiboUserId(command.userId());
+        this.deviceType = DeviceTypes.valueOf(command.deviceType());
+        this.modelName = command.modelName();
+        this.serialNumber = new SerialNumber(command.serialNumber());
+        this.macAddress = new MacAddress(command.macAddress());
+        this.firmwareVersion = new Version(command.firmwareVersion());
+        this.installationDate = command.installationDate();
+        this.roomLocation = command.roomLocation();
+    }
 }
