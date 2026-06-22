1a391ee934398e1b4b3116039425ba42b13b63f2
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-22 14:18:25 -0500

    feat(infrastructure): add DevicePersistenceAssembler.java

---
+++ src/main/java/com/sensibo/platform/u202416903/registry/infrastructure/persistence/jpa/assemblers/DevicePersistenceAssembler.java
@@ -0,0 +1,37 @@
+package com.sensibo.platform.u202416903.registry.infrastructure.persistence.jpa.assemblers;
+
+import com.sensibo.platform.u202416903.registry.domain.model.aggregates.Device;
+import com.sensibo.platform.u202416903.registry.infrastructure.persistence.jpa.entities.DevicePersistenceEntity;
+
+public final class DevicePersistenceAssembler {
+    private DevicePersistenceAssembler() {}
+
+    public static DevicePersistenceEntity toPersistenceFromDomain(Device device) {
+        DevicePersistenceEntity devicePersistenceEntity = new DevicePersistenceEntity();
+        devicePersistenceEntity.setId(device.getId());
+        devicePersistenceEntity.setUserId(device.getUserId());
+        devicePersistenceEntity.setDeviceType(device.getDeviceType());
+        devicePersistenceEntity.setModelName(device.getModelName());
+        devicePersistenceEntity.setSerialNumber(device.getSerialNumber());
+        devicePersistenceEntity.setMacAddress(device.getMacAddress());
+        devicePersistenceEntity.setFirmwareVersion(device.getFirmwareVersion());
+        devicePersistenceEntity.setInstallationDate(device.getInstallationDate());
+        devicePersistenceEntity.setRoomLocation(device.getRoomLocation());
+        devicePersistenceEntity.setDeviceType(device.getDeviceType());
+        return devicePersistenceEntity;
+    }
+    public static Device toDomainFromPersistence(DevicePersistenceEntity devicePersistenceEntity) {
+        Device device = new Device();
+        device.setId(devicePersistenceEntity.getId());
+        device.setUserId(devicePersistenceEntity.getUserId());
+        device.setDeviceType(devicePersistenceEntity.getDeviceType());
+        device.setModelName(devicePersistenceEntity.getModelName());
+        device.setSerialNumber(devicePersistenceEntity.getSerialNumber());
+        device.setMacAddress(devicePersistenceEntity.getMacAddress());
+        device.setFirmwareVersion(devicePersistenceEntity.getFirmwareVersion());
+        device.setInstallationDate(devicePersistenceEntity.getInstallationDate());
+        device.setRoomLocation(devicePersistenceEntity.getRoomLocation());
+        device.setDeviceType(devicePersistenceEntity.getDeviceType());
+        return device;
+    }
+}
